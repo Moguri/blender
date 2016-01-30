e6f4877af015162a66298f17ad13f146c4e8bd54
Mitchell Stokes <mogurijin@gmail.com>
2016-01-29 21:43:06 -0800

    BGE Rework: Removing VideoTexture
    This code relies heavily on the rasterizer, which is about to be
    removed. We have a copy in the game engine folder. We'll also want to
    look into implementing some VideoTexture features into Blender and it's
    viewport.

--- source/ketsji/Ketsji/KX_PythonInit.cpp
+++ source/ketsji/Ketsji/KX_PythonInit.cpp
@@ -1950,11 +1950,6 @@ PyMODINIT_FUNC initBGE(void)
 	PyDict_SetItemString(sys_modules, mod_full, submodule);
 	Py_INCREF(submodule);
 
-	mod_full = "bge.texture";
-	PyModule_AddObject(mod, SUBMOD, (submodule = initVideoTexturePythonBinding()));
-	PyDict_SetItemString(sys_modules, mod_full, submodule);
-	Py_INCREF(submodule);
-
 	mod_full = "bge.types";
 	PyModule_AddObject(mod, SUBMOD, (submodule = initGameTypesPythonBinding()));
 	PyDict_SetItemString(sys_modules, mod_full, submodule);
